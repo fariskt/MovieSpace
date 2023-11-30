17a14e6f7ac3e53c4357b8b7c58a371822749c06
Fariskt <fariskt00@gmail.com>
2023-11-30 13:44:31 +0530

    media style changed

--- src/Components/MovieDetails/MovieDetalis.jsx
+++ src/Components/MovieDetails/MovieDetalis.jsx
@@ -13,14 +13,11 @@ const MovieDetalis = ({openModal, setOpenModal}) => {
       <div className='year'>
           <p>year : {movie?.Year}</p>
         </div>
+          <div className='title'>
+            <h3>Name : {movie?.Title}</h3>
+          </div>
         <div className='image'>
-          <img style={{
-            height: "400px",
-            width: "300px"
-          }} src={movie?.Poster !== "N/A" ? movie?.Poster : "https://via.placeholder.com/400"} alt={movie?.Title} />
-        </div>
-        <div className='title'>
-          <h3>Name : {movie?.Title}</h3>
+          <img className="img" src={movie?.Poster !== "N/A" ? movie?.Poster : "https://via.placeholder.com/400"} alt={movie?.Title} />
         </div>
       </div>
     </Modal>
